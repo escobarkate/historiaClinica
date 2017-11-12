2a060e4c900b8d3e4ec9da1ff9496250d618ee63
Katherin Escobar <escobarkathe@gmail.com>
2017-11-12 18:53:48 -0500

    Buscar

--- src/pages/historia_clinica/buscar-pa/buscar-pa.ts
+++ src/pages/historia_clinica/buscar-pa/buscar-pa.ts
@@ -15,54 +15,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class BuscarPaPage {
   searchQuery: string = '';
-  items: string[];
+  nombres: string[];
+  cedulas: string[];
+  paciente: Array<{cedula: string, nombre: string}>;
+  buscar: string = "cedula";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
      this.initializeItems();
   }
      initializeItems() {
-    this.items = [
-      'Amsterdam',
-      'Bogota',
-      'Buenos Aires',
-      'Cairo',
-      'Dhaka',
-      'Edinburgh',
-      'Geneva',
-      'Genoa',
-      'Glasglow',
-      'Hanoi',
-      'Hong Kong',
-      'Islamabad',
-      'Istanbul',
-      'Jakarta',
-      'Kiel',
-      'Kyoto',
-      'Le Havre',
-      'Lebanon',
-      'Lhasa',
-      'Lima',
-      'London',
-      'Los Angeles',
-      'Madrid',
-      'Manila',
-      'New York',
-      'Olympia',
-      'Oslo',
-      'Panama City',
-      'Peking',
-      'Philadelphia',
-      'San Francisco',
-      'Seoul',
-      'Taipeh',
-      'Tel Aviv',
-      'Tokio',
-      'Uelzen',
-      'Washington'
+      // this.paciente=[{cedula: '1 ' , nombre: 'Mauricio Velez'},{cedula: '123' , nombre: 'Lorena Reyes'}];
+   this.nombres = [
+      'Karina Lopez',
+      'Felipe Torres',
+      'Katherin Escobar'   
+    ];
+    this.cedulas = [
+      '123',
+      '413',
+      '127'   
     ];
   }
 
-  getItems(ev) {
+  getItemsN(ev) {
     // Reset items back to all of the items
     this.initializeItems();
 
@@ -71,7 +46,22 @@ export class BuscarPaPage {
 
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
+      this.nombres = this.nombres.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  getItemsC(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.cedulas = this.cedulas.filter((item) => {
         return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
